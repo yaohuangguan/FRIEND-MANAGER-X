705a452292f4573951ec52b1c2527feb5217e675
yaohuangguan <yaob@miamioh.edu>
2019-09-21 09:21:28 +0800

    innitial

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export default () => (
+  <Fragment>
+    <div class='spinner-border' role='status'>
+      <span class='sr-only'>Loading...</span>
+    </div>
+  </Fragment>
+);
